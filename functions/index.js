ef481ec8004b6a6f8dae0b48d044af5bd3d84a85
Simone Di Fonzo <difonzosimone@hotmail.com>
2023-01-16 17:59:14 +0200

    Save III
     - Merge API

--- functions/index.js
+++ functions/index.js
@@ -35,29 +35,34 @@ let mobilenet;
 app.post("/admin/train", (req, res) => {
 	(async () => {
 		try {
-            await onStart();
+			await onStart();
 
-            let request = req.body;
+			let request = req.body;
 
-            let name = request.name;
+			let name = request.name;
 
-            let imageB64 = request.image;
-            let imageBuffer = Buffer.from(imageB64, "base64");
+			let imageB64 = request.image;
+			let imageBuffer = Buffer.from(imageB64, "base64");
 
-            var image = tf.node.decodeImage(imageBuffer);
+			var image = tf.node.decodeImage(imageBuffer);
 
-            let logits;
-            // 'conv_preds' is the logits activation of MobileNet.
-            const infer = () => mobilenet.infer(image, "conv_preds");
+			let logits;
+			// 'conv_preds' is the logits activation of MobileNet.
+			const infer = () => mobilenet.infer(image, "conv_preds");
 
-            logits = infer();
+			logits = infer();
 
-            // Add current image to classifier
-            knn.addExample(logits, name);
+			// Add current image to classifier
+			knn.addExample(logits, name);
 
 			return res
 				.status(200)
-				.send(JSON.stringify({ status: "Success", message: "Network trained successfully" }));
+				.send(
+					JSON.stringify({
+						status: "Success",
+						message: "Network trained successfully",
+					})
+				);
 		} catch (error) {
 			return res
 				.status(500)
@@ -69,35 +74,37 @@ app.post("/admin/train", (req, res) => {
 app.post("/whoami", (req, res) => {
 	(async () => {
 		try {
-            await onStart();
+			await onStart();
 
-            let request = req.body;
-            let imageB64 = request.image;
-            let imageBuffer = Buffer.from(imageB64, "base64");
-            var image = tf.node.decodeImage(imageBuffer);
+			let request = req.body;
+			let imageB64 = request.image;
+			let imageBuffer = Buffer.from(imageB64, "base64");
+			var image = tf.node.decodeImage(imageBuffer);
 
-            let logits;
-            // 'conv_preds' is the logits activation of MobileNet.
-            const infer = () => mobilenet.infer(image, "conv_preds");
+			let logits;
+			// 'conv_preds' is the logits activation of MobileNet.
+			const infer = () => mobilenet.infer(image, "conv_preds");
 
-            logits = infer();
+			logits = infer();
 
-            const numClasses = knn.getNumClasses();
-            if (numClasses > 0) {
-                // If classes have been added run predict
-                logits = infer();
-                const resPredict = await knn.predictClass(logits, TOPK);
+			const numClasses = knn.getNumClasses();
+			if (numClasses > 0) {
+				// If classes have been added run predict
+				logits = infer();
+				const resPredict = await knn.predictClass(logits, TOPK);
 
-                // var classIndex = resPredict.classIndex;
-                var label = resPredict.label;
-                // var confidence = resPredict.confidences[0];
+				// var classIndex = resPredict.classIndex;
+				var label = resPredict.label;
+				// var confidence = resPredict.confidences[0];
 
-                console.log(resPredict);
-            } else {
-                return res
-                    .status(500)
-                    .send(JSON.stringify({ status: "Error", message: "No models are here!" }));
-            }
+				console.log(resPredict);
+			} else {
+				return res
+					.status(500)
+					.send(
+						JSON.stringify({ status: "Error", message: "No models are here!" })
+					);
+			}
 
 			return res
 				.status(200)
@@ -111,12 +118,10 @@ app.post("/whoami", (req, res) => {
 });
 
 async function onStart() {
-    if (knn === undefined)
-	    knn = knnClassifier.create();
+	if (knn === undefined) knn = knnClassifier.create();
 
-    if (mobilenet === undefined)
-	    mobilenet = await mobilenetModule.load();
+	if (mobilenet === undefined) mobilenet = await mobilenetModule.load();
 }
 
 // exports the api to firebase cloud functions
-exports.api = functions.region('europe-west1').https.onRequest(app);
+exports.api = functions.region("europe-west1").https.onRequest(app);
